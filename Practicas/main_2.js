b596bb34ca1e144882eefed6253223e33a6fd9fb
Oscar E <a01660338@itesm.mx>
2021-09-30 17:45:21 -0500

    Added JS files

---
+++ Practicas/main_2.js
@@ -0,0 +1,17 @@
+var name = 'Carlos'; // con comillas simples
+    numero1 = 1,
+    numero2 = 7;
+
+alert(numero1 + numero2);
+
+document.write(numero1); // Escribe en el doc
+
+// En js se pueden concatenar strings, en general se usan comillas simples
+
+console.log('Hola consola'); // comillas simples
+
+for(var x = 0; x <= 10; x++){
+    document.write(numero2);
+    document.write('<br>'); // Se puede inscrustrar código html :O
+                            // Pero es mala practica
+} 
